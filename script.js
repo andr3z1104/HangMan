bcf56c6266ab8a576944f5134628f49a2e6c43d3
andr3z1104 <moreno.andres@correo.unimet.edu.ve>
2024-05-23 20:46:51 -0400

    Cambio total de la semantica y estructura de HTML y CSS, codigo boton JS

---
+++ script.js
@@ -0,0 +1,9 @@
+function ocultarBoton() {
+    var boton = document.getElementById("button_new_game");
+    boton.style.display = "none";
+  }
+
+function reiniciarJuego() {
+    var boton = document.getElementById("button_new_Game");
+    boton.style.display = "block";
+  }
