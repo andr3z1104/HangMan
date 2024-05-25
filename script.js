79eed9d2461cacab25220b7b2e98543a63be9228
andr3z1104 <moreno.andres@correo.unimet.edu.ve>
2024-05-25 12:24:16 -0400

    Creacion de las rayas segun la palabra, botones letras, y organizacion CSS HTML

--- script.js
+++ script.js
@@ -1,46 +1,62 @@
-(function() {
-  const letra = document.getElementById("contenerdor-letras");
-  const opciones_juegos = document.getElementById("opciones_juegos");
-  const input_usuario = document.getElementById("input_usuario");
-  const boton_juego_nuevo = document.getElementById("boton_juego_nuevo");
-  const juego_finalizado = document.getElementById("juego_finalizado");
-  const canvas = document.getElementById("canvas");
-  const resultado_final = document.getElementById("resultado_final");
-  const timer = document.getElementById("timer");
+
+const letra = document.getElementById("contenerdor-letras");
+const opciones_juegos = document.getElementById("opciones_juegos");
+const input_usuario = document.getElementById("input_usuario");
+const boton_juego_nuevo = document.getElementById("boton_juego_nuevo");
+const juego_finalizado = document.getElementById("juego_finalizado");
+const canvas = document.getElementById("canvas");
+const resultado_final = document.getElementById("resultado_final");
+const timer = document.getElementById("timer")
+
+boton_juego_nuevo.addEventListener('click', NewGame);
+
+const palabras = ['manzana', 'pera', 'banana', 'naranja', 'uva', 'sandía', 'kiwi', 'mango', 'fresa', 'piña'];
 
 
-  function ocultarBoton() {
-    var boton = document.getElementById("button_new_game");
-    boton.style.display = "none";
+function palabraRandom(len){
+  const valor_palabra = Math.floor(Math.random() * len);
+  return valor_palabra;
+}
+
+
+
+function ocultarBoton() {
+var boton = document.getElementById("boton_juego_nuevo");
+boton.style.display = "none";
+}
+
+
+function ocultarletra() {
+  var letras = document.getElementsByClassName("botones_letras");
+  for (var i = 0; i < letras.length; i++) {
+      letras[i].style.display = "none";
+  }
+}
+
+
+function reiniciarJuego() {
+  var boton = document.getElementById("button_new_Game");
+  boton.style.display = "block";
+}
+
+
+function NewGame(event){
+  const adivinar = document.getElementById("palabra_secreta");
+  adivinar.innerHTML= '';
+
+  const len_palabras = palabras.length;
+  const valor_palabra = palabraRandom(len_palabras);
+  const palabra = palabras[valor_palabra]
+  const len_palabra = palabra.length;
+
+  console.log(palabra);
+
+  for( let i =0; i < len_palabra; i++ ){
+    const raya = document.createElement('span');
+    adivinar.appendChild(raya);
   }
 
-  function ocultarletra() {
-    var letras = document.getElementsByClassName("botones_letras");
-    for (var i = 0; i < letras.length; i++) {
-        letras[i].style.display = "none";
-    }
-  }
-
-  function crearListaAZ() {
-  var lista = [];
-  for (var i = 65; i <= 90; i++) {
-    var letra = String.fromCharCode(i);
-    lista.push(letra);
-  }
-  return lista;
-  }
-  var listaAZ = crearListaAZ();
-  console.log(listaAZ);
-
-
-
-  function reiniciarJuego() {
-    var boton = document.getElementById("button_new_Game");
-    boton.style.display = "block";
-  }
-  console.log("¡Hola desde la función autoejecutable!");
-})();
-
+}
 
 
 
